6d01d8e00741375c749f4d47aaa4ac793bca7613
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-02 20:31:23 -0500

    vertical bar finish

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,15 @@
-import sun from "../images/sun.png"
+import sun from "../images/sun.png";
 
 const Navbar = () => {
   return (
     <section className="navbar" id="navbar">
-      <nav className="nav" id="nav">
-          <img className="sunicon" src={sun}></img>
-          <div className="navlinks">
-              <p>Home</p>
-              <p>About</p>
-              <p>Gallery</p>
-              <p>Contact</p>
-          </div>
-      </nav>
+      <div className="nav-items">
+        <p>Home</p>
+        <p>About</p>
+        <p>Gallery</p>
+        <p>Contact</p>
+        <img className="sunicon" src={sun}></img>
+      </div>
     </section>
   );
 };
--- src/components/verticalbar.jsx
+++ src/components/verticalbar.jsx
@@ -1,7 +1,20 @@
 const VerticalBar = () => {
   return (
     <section className="verticalbar" id="verticalbar">
-      <h2>VerticalBar</h2>
+      <h2>Xae</h2>
+      <div className="elastic"></div>
+      <div className="social-media">
+        <a href="https://www.facebook.com/hstrejoluna" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://www.instagram.com/hstrejoluna" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter"></i>
+        </a>
+
+      </div>
     </section>
   );
 };
