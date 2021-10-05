a36bc63f92e2f033220eb1314d9924a40bf7c6bf
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-04 21:00:57 -0500

    background fill footer rectangle

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,19 +4,15 @@ import arrow from "../images/arrowfooter.png";
 const Footer = () => {
   return (
     <section className="footer" id="footer">
-      <div className="mainarticle">
-        <img src={footerimg}></img>
-        <h3>Exploration</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat id
-          potenti faucibus nec, rhoncus, vulputate
-        </p>
-        <img src={arrow}></img>
-      </div>
-      <div className="secondarticle">
-        <h3>Footprint</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-      </div>
+      <img id="mainarticleimg" src={footerimg}></img>
+      <h3 id="mainarticleh3">Exploration</h3>
+      <p id="mainarticlep">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat id
+        potenti faucibus nec, rhoncus, vulputate
+      </p>
+      <img id="mainarticlearrow" src={arrow}></img>
+      <h3 id="secondarticleh3">Footprint</h3>
+      <p id="secondarticlep">Lorem ipsum dolor sit amet, consectetur </p>
     </section>
   );
 };
